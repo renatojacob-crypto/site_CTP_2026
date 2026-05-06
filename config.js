6f137558c77e7981aaf88d948b99b7c570fa7ba6
renatojacob-crypto <renato.jacob@yxzrobotica.com.br>
2026-05-05 21:50:27 -0300

    Update config.js

--- config.js
+++ config.js
@@ -2,4 +2,4 @@
 // Cole aqui a URL do Power Automate quando o fluxo estiver pronto.
 // Enquanto estiver vazio, o formulário salvará os registros apenas no navegador, para testes.
 
-const POWER_AUTOMATE_URL = "https://defaultf916220e9d9a444a9c2bddd8cd5534.b2.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/eba2173d84b64a9aa6d8c99be2157d33/triggers/manual/paths/invoke?api-version=1";
+const POWER_AUTOMATE_URL = "https://defaultf916220e9d9a444a9c2bddd8cd5534.b2.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/eba2173d84b64a9aa6d8c99be2157d33/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=8T9OnLAlCl1Sv998JSrKikiII8Wey6obW0F-geKjCyE";
